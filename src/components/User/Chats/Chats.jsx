b3898d2398b00220216c07cdf58c43439ab161f6
shakilshagu <muhammadshakil1968@gmail.com>
2023-09-14 11:06:22 +0530

    new commit

--- src/components/User/Chats/Chats.jsx
+++ src/components/User/Chats/Chats.jsx
@@ -22,7 +22,11 @@ const Chats = () => {
         { text: message, sender },
         { headers }
       );
-      socket.emit("message", { text: message, sender }, { headers });
+      socket.emit(
+        "message",
+        { text: message, sender },
+        { headers }
+      );
       setMessage("");
       if (response.data.success) {
         toast.success(response.data.message);
@@ -37,10 +41,12 @@ const Chats = () => {
 
   const getmessage = async () => {
     try {
-      const response = await AxiosUser.get(`getmessage`, { headers }, {});
+      const response = await AxiosUser.get(`getmessage`, { headers });
       if (response.data.messages) {
-        setMessages(response.data.messages);
-        console.log(response.data.messages);
+        const sortedMessages = response.data.messages.sort(
+          (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+        );
+        setMessages(sortedMessages);
         toast.success(response.data.message);
       } else {
         setMessages([]);
@@ -55,7 +61,6 @@ const Chats = () => {
   useEffect(() => {
     getmessage();
     const socket = io(ServerPort);
-
     socket.on("connect", () => {
       console.log("User connected to socket.io");
     });
@@ -74,40 +79,37 @@ const Chats = () => {
   return (
     <div className="overflow-y-hidden">
       <Header />
-      <form action="">
-        <body className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
-          <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
-            <div className="flex  flex-grow h-0 p-4 overflow-auto flex-wrap-reverse">
-              {messages.map((chat, index) => (
-                <div
-                  key={index}
-                  className={`flex w-[100%] mt-2 space-x-3 ${
-                    chat.senter === "Admin"
-                      ? "flex justify-start"
-                      : "flex justify-end"
-                  }`}
-                >
-                  {chat.senter === "Admin" && (
-                    <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-                  )}
 
-                  <div>
-                    <div
-                      className={` p-3 ${
-                        chat.senter === "Admin"
-                          ? "rounded-r-lg rounded-bl-lg bg-gray-300"
-                          : " bg-blue-300 rounded-l-lg rounded-br-lg text-white"
-                      }  `}
-                    >
-                      <p className="text-sm">{chat?.text}</p>
-                    </div>
-                    <span className="text-xs text-gray-500 leading-none">
-                      2 min ago
-                    </span>
+      <body className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+        <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
+          <div className="flex  flex-grow h-0 p-4 overflow-auto flex-wrap-reverse">
+            {messages.slice().reverse().map((chat, index) => (
+              <div
+                key={index}
+                className={`flex w-[100%] mt-2 space-x-3 ${
+                  chat.senter === "Admin" ? "justify-start mb-2" : "justify-end"
+                }`}
+              >
+                {chat.senter === "Admin" && (
+                  <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                )}
+
+                <div>
+                  <div
+                    className={` p-3 ${
+                      chat.senter === "Admin"
+                        ? "rounded-r-lg rounded-bl-lg bg-gray-300"
+                        : " bg-blue-300 rounded-l-lg rounded-br-lg text-white"
+                    }  `}
+                  >
+                    <p className="text-sm ">{chat?.text}</p>
                   </div>
+                  <span className="text-xs text-gray-500 leading-none"></span>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
+          </div>
+          <form action="">
             <div className="bg-gray-300 p-4 flex items-center">
               <input
                 className="flex-grow h-10 rounded px-3 text-sm"
@@ -123,9 +125,10 @@ const Chats = () => {
                 Send
               </button>
             </div>
-          </div>
-        </body>
-      </form>
+          </form>
+        </div>
+      </body>
+
       <Footer />
     </div>
   );
