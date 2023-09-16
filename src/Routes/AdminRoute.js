27d1f564c8ee4abd9b204659c108a9b2a69a4289
shakilshagu <muhammadshakil1968@gmail.com>
2023-09-16 06:42:01 +0530

    new commit

--- src/Routes/AdminRoute.js
+++ src/Routes/AdminRoute.js
@@ -22,9 +22,7 @@ function AdminRoute() {
           path="/*"
           element={
             <ProtectedRouteAdmin>
-              {""}
               <Dashbord />
-              {""}
             </ProtectedRouteAdmin>
           }
         />
